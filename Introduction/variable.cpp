d987c1a7f1ecb033d12a334c43574a7477eef649
Ayon Biswas <ayon9901s@gmail.com>
2025-05-11 01:15:32 +0600

    variable added

---
+++ Introduction/variable.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    char name;
+    float avg;
+    bool isIt;
+    age =24;
+    name='ayon';
+    avg=14.5;
+    isIt=true;
+    cout<<age<<endl<<name<<endl<<avg<<endl<<isIt<<endl;
+
+
+return 0;
+}
