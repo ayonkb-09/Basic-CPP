ed321ad01f85fe6eedee4b5f98fa9b832429affc
Ayon Biswas <ayon9901s@gmail.com>
2025-05-13 00:09:40 +0600

    short hand operator added

---
+++ Introduction/short_hand_operator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int num1=20,num2=10,num3=40,num4=50,num5=60,num6=70,num7=90,num8=10;
+   num1+=num2;
+   num3-=num4;
+   num5*=num6;
+   num7/=num8;
+   cout<<num1<<endl;
+   cout<<num3<<endl;
+   cout<<num5<<endl;
+   cout<<num7<<endl;
+    return 0;
+}
