181f9139ea2faa0360b9a66c81530c3073a474cc
Ayon Biswas <ayon9901s@gmail.com>
2025-05-13 00:47:44 +0600

    if else condition added

---
+++ Conditionals/if_else.cpp
@@ -0,0 +1,23 @@
+/*if(condition)
+{
+statement1;
+}
+else{
+statement2;
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int age;
+   cout<< "Enter the age:";
+   cin>>age;
+   if(age>18)
+   {
+    cout<< "Able for vote.";
+   }
+   else{
+    cout<<"Unable";
+   }
+    return 0;
+}
