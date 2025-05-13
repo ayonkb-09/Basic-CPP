7990877c2039f281d919014d6a897cc9b2849e86
Ayon Biswas <ayon9901s@gmail.com>
2025-05-13 23:21:53 +0600

    Ternary operator added

---
+++ Conditionals/ternary_operator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int mark;
+   cout<< "Enter the marks:";
+   cin>>mark;
+   mark>=33?cout<<"You are passed":cout<<"you are not";
+   return 0;
+}
